986b255d9ae2b8a47cc15af59eb461ec98dc5eae
Roman Hargrave <rhargrave@taxsaverplan.com>
2018-10-22 16:53:41 -0500

    Initial

---
+++ common.py
@@ -0,0 +1,97 @@
+import time
+
+from imapclient import IMAPClient
+from threading import Thread
+
+import logging
+import atexit
+
+# Thread that sends NOOP to keep the server happy
+class KeepAliveWorker(Thread):
+    def __init__(self, client):
+        self.client = client
+        super(KeepAliveWorker, self).__init__()
+
+    def run(self):
+        try:
+            self.client.noop()
+            time.sleep(2)
+        except:
+            # a lazy way to deal with the inevitable exception on logout()
+            # rather than using signaling.
+            pass
+
+# Message; holds a single endicia id and retrieves contents when asked
+class Endicia:
+    def __init__(self, client, message_id):
+        self.client     = client
+        self.message_id = message_id
+        self._r822      = None
+        self._envelope  = None
+
+    @property
+    def id(self):
+        return self.message_id
+
+    @property
+    def envelope(self):
+        if self._envelope is None:
+            res = self.client.fetch(self.message_id, [u'ENVELOPE'])
+            self._envelope = res[self.message_id][u'ENVELOPE']
+
+        return self._envelope
+
+    @property
+    def subject(self):
+        return self.envelope.subject
+
+    @property
+    def r822(self):
+        if self._r822 is None:
+            res = self.client.fetch(self.message_id, [u'RFC822'])
+            self._r822 = res[self.message_id][u'RFC822']
+
+        return self._r822
+
+# Folder; holds a list of endicia ids, sorted in ascending order
+class Folder:
+    def __init__(self, client, name, criteria = u'ALL'):
+        self.client   = client
+        self.name     = name
+        self.criteria = criteria
+        self._endicia = None
+
+    @property
+    def endicia(self):
+        if self._endicia is None:
+            self.client.select_folder(self.name)
+            self._endicia = sorted(map((lambda num: Endicia(self.client, num)), self.client.search(self.criteria)), key=lambda x: x.id)
+
+        return self._endicia
+
+# Account; holds a list of folder names
+class Connection:
+    def __init__(self, server, username, password, ssl=True):
+        self.server     = server
+        self.username   = username
+        self.password   = password
+        self.use_ssl    = ssl
+        self._client    = None
+        self._folders   = None
+
+    @property
+    def client(self):
+        if self._client is None:
+            self._client = IMAPClient(host=self.server, ssl=self.use_ssl)
+            self._client.login(self.username, self.password)
+            atexit.register(self._client.logout)
+            #KeepAliveWorker(self._client).start()
+
+        return self._client
+
+    @property
+    def folders(self):
+        if self._folders is None:
+            self._folders = map((lambda data: Folder(self.client, data[2])), self.client.list_folders())
+
+        return self._folders
---
+++ imap2group.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2.7
+from common import Connection
+import common
+
+import logging
+
+import io
+import os
+import sys
+import time
+import argparse
+import traceback
+
+# Google API deps
+import apiclient
+import httplib2
+import oauth2client
+
+SCOPES = [ 'https://www.googleapis.com/auth/apps.groups.migration' ]
+
+def connect_service(args):
+    credential_dir = os.path.dirname(args.credential_file)
+
+    if not os.path.isdir(credential_dir):
+        os.makedirs(credential_dir)
+
+    cred_store = oauth2client.file.Storage(args.credential_file)
+    credentials = cred_store.get()
+
+    if not credentials or credentials.invalid:
+        flow = oauth2client.client.flow_from_clientsecrets(args.client_secret_file, SCOPES)
+        credentials = oauth2client.tools.run_flow(flow, cred_store, args)
+
+    http = credentials.authorize(httplib2.Http())
+    return apiclient.discovery.build('groupsmigration', 'v1', http=http)
+
+# upload to group
+def upload_folder(log, service, args, folder):
+    archive = service.archive()
+
+    failure_count   = 0
+    success_count   = 0
+    total_count     = len(folder.endicia)
+    print_countdown = args.print_after
+
+    log.info('Uploading folder "%s", with %d endicia' % (folder.name, total_count))
+
+    for message in folder.endicia:
+        try:
+            strio = io.BytesIO(message.r822)
+            media = apiclient.http.MediaIoBaseUpload(strio, mimetype='message/rfc822')
+            time.sleep(1.0 / args.rate_limit)
+            result = archive.insert(groupId=args.group, media_body=media).execute()
+            assert result['responseCode'].lower() == 'success'
+            log.debug('Uploaded indicia #%d' % (message.id))
+
+        except Exception as ex:
+            log.exception('Failed to upload message with IMAP ID %d in folder "%s"; subject "%s"' % (message.id, folder.name, message.subject))
+            failure_count += 1
+
+        print_countdown -= 1
+        if print_countdown == 0:
+            log.info("... uploaded %d of %d endicia in %s (errors = %d)" % (success_count, total_count, folder.name, failure_count))
+
+    log.info('... folder "%s" completed: out of %d messages, %d succeeded and %d failed' % (folder.name, total_count, success_count, failure_count))
+
+def main():
+    parser = argparse.ArgumentParser(parents = [oauth2client.tools.argparser], description = 'Migrate an IMAP account to Google Groups')
+    parser.add_argument('server',           help='IMAP Server')
+    parser.add_argument('username',         help='IMAP Username')
+    parser.add_argument('password',         help='IMAP Password')
+    parser.add_argument('group',            help='Google Group Address')
+    parser.add_argument('--verbose',        dest='verbose', action='store_true', help='Detailed (read: trace level) logging')
+    parser.add_argument('--ssl',            dest='use_ssl', action='store_true', help='Use SSL (Requires Valid Cert)')
+    parser.add_argument('--report',         dest='print_after', type=int, default=100, help='Print status every N endicia (per folder)')
+    parser.add_argument('--rate',           dest='rate_limit', type=int, default=10, help='API Rate Limit')
+    parser.add_argument('--cid',            dest='client_secret_file', default='client_secret.json')
+    parser.add_argument('--cred-store',     dest='credential_file', default=os.path.join(os.path.expanduser('~'), '.google', 'imap2group.json'))
+
+    logging.basicConfig(format = '[%(asctime)s %(name)s %(levelname)s] %(message)s')
+
+    log  = logging.getLogger('imap2group')
+    args = parser.parse_args()
+
+    if (args.verbose):
+        log.setLevel(logging.DEBUG)
+
+
+    connection  = Connection(args.server, args.username, args.password, ssl=args.use_ssl)
+    service     = connect_service(args)
+
+    for folder in connection.folders:
+        upload_folder(log, service, args, folder)
+
+if __name__ == "__main__":
+    main()
+
