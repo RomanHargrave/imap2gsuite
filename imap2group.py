c9808582f9f89484d3095fbb237e2cfe852df00e
Roman Hargrave <rhargrave@taxsaverplan.com>
2019-02-05 15:10:40 -0600

    Move to queue/loader architecture

--- imap2group.py
+++ imap2group.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python2.7
 from common import Connection
 import common
+import threading
+import queue
+
+from datetime import datetime, timedelta
 
 import logging
 
@@ -36,37 +40,77 @@ def connect_service(args):
     http = credentials.authorize(httplib2.Http())
     return apiclient.discovery.build('groupsmigration', 'v1', http=http)
 
-# upload to group
-def upload_folder(log, service, args, folder):
-    archive = service.archive()
+class MailProcessor(threading.Thread):
+    def __init__(self, log, service, args):
+        self.log            = log
+        self.mailq          = queue.Queue()
+        self.service        = service
+        self.alive          = True
+        self.args           = args
+        self._success_count = 0
+        self._failure_count = 0
+        super(MailProcessor, self).__init__()
 
-    failure_count   = 0
-    success_count   = 0
-    total_count     = len(folder.mailpieces)
-    print_countdown = args.print_after
+    def qsize(self):
+        return self.mailq.qsize()
 
-    log.info('Uploading folder "%s", with %d mailpieces' % (folder.name, total_count))
+    @property
+    def failure_count(self):
+        return self._failure_count
 
-    for message in folder.mailpieces:
-        try:
-            strio = io.BytesIO(message.r822)
-            media = apiclient.http.MediaIoBaseUpload(strio, mimetype='message/rfc822')
-            time.sleep(1.0 / args.rate_limit)
-            result = archive.insert(groupId=args.group, media_body=media).execute()
-            assert result['responseCode'].lower() == 'success'
-            log.debug('Uploaded mailpieces #%d' % (message.id))
-            success_count += 1
+    @property
+    def success_count(self):
+        return self._success_count
 
-        except Exception as ex:
-            log.exception('Failed to upload message with IMAP ID %d in folder "%s"; subject "%s"' % (message.id, folder.name, message.subject))
-            failure_count += 1
+    def submit(self, mailpiece):
+        self.mailq.put((mailpiece, 0))
 
-        print_countdown -= 1
-        if print_countdown == 0:
-            log.info("... uploaded %d of %d mailpieces in %s (errors = %d)" % (success_count, total_count, folder.name, failure_count))
-            print_countdown = args.print_after
+    def stop(self):
+        self.alive = False
 
-    log.info('... folder "%s" completed: out of %d messages, %d succeeded and %d failed' % (folder.name, total_count, success_count, failure_count))
+    def run(self):
+        arch            = self.service.archive()
+        print_countdown = self.args.print_after
+
+        while self.alive:
+            try:
+                (message, attempts) = self.mailq.get(block=True,timeout=2);
+
+                self.log.info('Processing mailpiece %d, attempt number %d' % (message.id, attempts))
+
+                strio = io.BytesIO(message.r822)
+                media = apiclient.http.MediaIoBaseUpload(strio, mimetype='message/rfc822')
+                time.sleep(1.0 / self.args.rate_limit)
+                result = arch.insert(groupId=self.args.group, media_body=media).execute()
+                assert result['responseCode'].lower() == 'success'
+                self.log.debug('Uploaded mailpiece %d after %d attempts' % (message.id, attempts))
+                self._success_count += 1
+
+            except queue.Empty as qex:
+                continue
+
+            except Exception as ex:
+                subject = "(error)"
+                try:
+                    subject = message.subject
+
+                except Exception as qex:
+                    pass
+
+                if attempts < self.args.retries:
+                    self.log.debug('Message upload failed for message ID %d; re-enqueueing' % (message.id))
+                    self.mailq.put((message, attempts + 1))
+                else:
+                    self.log.exception('Failed to upload message %d after %d attempts; subject=%s' % (message.id, attempts, subject))
+                    self._failure_count += 1
+
+            self.mailq.task_done()
+
+            print_countdown -= 1
+
+            if print_countdown == 0:
+                print_countdown = self.args.print_after
+                self.log.info('... stats: success=%d; failures=%d; queue=%d' % (self.success_count, self.failure_count, self.mailq.qsize()))
 
 def main():
     parser = argparse.ArgumentParser(parents = [oauth2client.tools.argparser], description = 'Migrate an IMAP account to Google Groups')
@@ -76,28 +120,60 @@ def main():
     parser.add_argument('group',            help='Google Group Address')
     parser.add_argument('--verbose',        dest='verbose', action='store_true', help='Detailed (read: trace level) logging')
     parser.add_argument('--ssl',            dest='use_ssl', action='store_true', help='Use SSL (Requires Valid Cert)')
-    parser.add_argument('--report',         dest='print_after', type=int, default=100, help='Print status every N mailpieces (per folder)')
+    parser.add_argument('--ssl-noverify',   dest='verify_ssl', action='store_false', help='Use SSL (Requires Valid Cert)')
+    parser.add_argument('--report',         dest='print_after', type=int, default=50, help='Print status every N mailpieces (per folder)')
     parser.add_argument('--rate',           dest='rate_limit', type=int, default=10, help='API Rate Limit')
     parser.add_argument('--cid',            dest='client_secret_file', default='client_secret.json')
     parser.add_argument('--cred-store',     dest='credential_file', default=os.path.join(os.path.expanduser('~'), '.google', 'imap2group.json'))
-    parser.add_argument('--imaplib-maxline',dest='maxline', default=25000, help='Value to set imaplib._MAXLINE to')
+    parser.add_argument('--imaplib-maxline',dest='maxline', type=int, default=25000, help='Value to set imaplib._MAXLINE to')
+    parser.add_argument('--retries',        dest='retries', type=int, default=3, help='Number of retries for a message')
+    parser.add_argument('--max-pressure',   dest='qpressure', type=int, default=6000, help='Maximum queue pressure')
 
     logging.basicConfig(format = '[%(asctime)s %(name)s %(levelname)s] %(message)s')
 
     log  = logging.getLogger('imap2group')
     args = parser.parse_args()
 
+    imaplib._MAXLINE = args.maxline
+
     if (args.verbose):
         log.setLevel(logging.DEBUG)
 
-
-    connection  = Connection(args.server, args.username, args.password, ssl=args.use_ssl)
-    service     = connect_service(args)
+    connection   = Connection(args.server, args.username, args.password, ssl = args.use_ssl, verify_ssl = args.verify_ssl)
+    service      = connect_service(args)
+    expect_count = 0
+    start_time   = time.time()
+    processor    = MailProcessor(log, service, args)
+    processor.start()
 
     for folder in connection.folders:
-        upload_folder(log, service, args, folder)
+        try:
+            folder_size = len(folder.mailpieces)
+            expect_count += folder_size
+            log.info('Enqueueing folder %s (%d messages in folder, %d messages currently enqueued)' % (folder.name, folder_size, processor.qsize()))
+            for message in folder.mailpieces:
+                while processor.qsize() >= args.qpressure:
+                    time.sleep(.3)
 
-    imaplib._MAXLINE = maxline
+                processor.submit(message)
+        except KeyboardInterrupt as ki:
+            log.info('Asking processor to exit')
+            processor.stop()
+            break;
+
+        except Exception as ex:
+            log.exception('Failed to enqueue folder %s. Maybe an IMAP error ocurred?' % (folder.name))
+
+    log.info('Waiting on remaining uploads...');
+    processor.join()
+
+    end_time = time.time()
+    duration = datetime(1,1,1) + timedelta(seconds=int(end_time - start_time))
+    duration_text = '%dd %dh %dm %ds' % (duration.day - 1, duration.hour, duration.minute, duration.second)
+
+
+
+    log.info('Summary: ok=%d; fail=%d; total=%d; expected=%d; time=%s' % (processor.success_count, processor.failure_count, processor.success_count + processor.failure_count, expect_count, duration_text))
 
 if __name__ == "__main__":
     main()
